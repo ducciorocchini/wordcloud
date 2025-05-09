6f3493cff8155dafec54bc4d5fdb7038fa41a104
Duccio Rocchini <ducciorocchini@gmail.com>
2025-05-09 10:38:53 +0200

    Update script.R

--- script.R
+++ script.R
@@ -2,23 +2,24 @@ library(wordcloud)
 library(RColorBrewer)
  
 # Define the words and their frequency
-# words <- c("Spatial", "Visualization", "Ecolinformatics", "Theoretical", 
-           "Computational", "Open", "Remote")
+words <- c("Spatial", "Visualization", "Ecolinformatics", "Theoretical", 
+           "Computational", "Open", "Remote", "Ecology", "Maths", "Maps")
 
 # Define other words and their frequency
-words <- c("Running", "Art", "Books", "Countryside", 
+# words <- c("Running", "Art", "Books", "Countryside", 
            "Trips", "Oca", "Mountain")
 
 # Assigning the frequency of each word (here, just assuming each word has a frequency of 1 for simplicity)
 # frequencies <- rep(1, length(words))
 # Or different frequencies
-frequencies <- c(25, 23, 20, 19, 20, 6, 18)  # Each word gets a different frequency
+frequencies <- c(25, 23, 20, 19, 
+                 20, 6, 18, 15, 16, 10)  # Each word gets a different frequency
 
 # Colors
 # Use a predefined color palette (e.g., "Set3")
 # https://www.datanovia.com/en/blog/the-a-z-of-rcolorbrewer-palette/
-# colors <- brewer.pal(7, "GnBu")  # 7 colors from the "Set3" palette
-colors <- brewer.pal(7, "OrRd")  # 7 colors from the "Set3" palette
+colors <- brewer.pal(7, "GnBu")  # 7 colors from the "Set3" palette
+# colors <- brewer.pal(7, "OrRd")  # 7 colors from the "Set3" palette
 
 
 # Create the word cloud with colors
@@ -28,7 +29,8 @@ colors <- brewer.pal(7, "OrRd")  # 7 colors from the "Set3" palette
 # Mixed:
 
 # Create the word cloud with both vertical and horizontal words and different colors
-pdf("outwords2.pdf")
+# pdf("outwords2.pdf")
+set.seed(42)
 wordcloud(words = words, freq = frequencies, min.freq = 1, scale = c(3, 0.5), 
           colors = colors, rot.per = 0.5)  # Rotate 50% of words vertically
-dev.off()
+# dev.off()
